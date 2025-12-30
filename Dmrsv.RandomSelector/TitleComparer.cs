3e379a77b03f95321aff01de52ae43388bd47117
Pali <hyunida0310@gmail.com>
2025-12-31 00:22:28 +0900

    Fix title sort

--- Dmrsv.RandomSelector/TitleComparer.cs
+++ Dmrsv.RandomSelector/TitleComparer.cs
@@ -17,14 +17,14 @@
                 return 1;
             }
             // Djmax sorts titles with case-insensitive and ignoring the characters below
-            x = x.Replace("'", string.Empty).Replace("-", string.Empty).ToUpper();
-            y = y.Replace("'", string.Empty).Replace("-", string.Empty).ToUpper();
+            x = CleanString(x);
+            y = CleanString(y);
             int index = x.Zip(y, (a, b) => a == b).TakeWhile(equals => equals).Count();
             if (index == Math.Min(x.Length, y.Length))
             {
                 return x.Length - y.Length;
             }
-            // priority order: white-space -> non-alphabetic letter -> special character -> number -> alphabet
+            // priority order: white-space -> non-alphabetic letter -> special character -> number -> alphabet(Normalize diacritics)
             char a = x[index], b = y[index];
             int priorityA = GetPriority(a, index);
             int priorityB = GetPriority(b, index);
@@ -33,6 +33,21 @@
                 return a.CompareTo(b);
             }
             return priorityA - priorityB;
+        }
+
+        private string CleanString(string text)
+        {
+            // Djmax sorts titles with case-insensitive and ignoring the characters below
+            string s = text.Replace("'", string.Empty).Replace("-", string.Empty).ToUpper();
+
+            // Djmax treats umlauts as their base alphabets for sorting
+            s = s.Replace("Ö", "O")
+                 .Replace("Ä", "A")
+                 .Replace("Ü", "U")
+                 .Replace("È", "E")
+                 .Replace("É", "E");
+            
+            return s;
         }
 
         private int GetPriority(char ch, int idx)
